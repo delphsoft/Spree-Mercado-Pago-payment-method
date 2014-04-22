ead883adb8bd1e9eb9cdffbcc23d2277b19a5cb5
Nicolás Peralta <nicolas@h1lab.com>
2014-04-22 17:24:00 -0300

    As we only have one payment method we hide the continue button

--- app/assets/javascripts/store/spree_mercado_pago_payment_method.js
+++ app/assets/javascripts/store/spree_mercado_pago_payment_method.js
@@ -1,15 +1,18 @@
 MercadoPago = {
     hidePaymentSaveAndContinueButton: function(paymentMethod) {
-        if (MercadoPago.paymentMethodID && paymentMethod.val() == MercadoPago.paymentMethodID) {
-            $('.continue').hide();
-        } else {
-            $('.continue').show();
-        }
+         $('.continue').hide();
+         $('.payment-method-selector').hide();
+        // if (MercadoPago.paymentMethodID && paymentMethod.val() == MercadoPago.paymentMethodID) {
+        //      $('.continue').hide();
+        // } else {
+        //     $('.continue').show();
+        // }
     }
 };
 
 $(document).ready(function() {
     checkedPaymentMethod = $('div[data-hook="checkout_payment_step"] input[type="radio"]:checked');
+    console.log(checkedPaymentMethod);
     MercadoPago.hidePaymentSaveAndContinueButton(checkedPaymentMethod);
     paymentMethods = $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function (e) {
         MercadoPago.hidePaymentSaveAndContinueButton($(e.target));
